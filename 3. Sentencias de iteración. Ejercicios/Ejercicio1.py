859cd71d686a4e27a99303cbe6b69a2d3831ea6a
Francisco Aybar <frannur1.ir@gmail.com>
2025-10-05 19:06:28 +0200

    Sentencias de iteración

---
+++ 3. Sentencias de iteración. Ejercicios/Ejercicio1.py
@@ -0,0 +1,9 @@
+# Escribe un programa que recoja un texto y escriba cada letra del texto 
+# en una línea distinta. 
+
+texto = input("Introduce un texto: ");
+
+print(texto[0]);
+
+
+print(len(texto));
