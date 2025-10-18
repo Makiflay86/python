7a5582a7a5b81435051b595ee4d36ee4af4eb4d5
Francisco Aybar <frannur1.ir@gmail.com>
2025-10-18 20:02:45 +0200

    Terminado

--- 3. Sentencias de iteración. Ejercicios/Ejercicio10.py
+++ 3. Sentencias de iteración. Ejercicios/Ejercicio10.py
@@ -3,37 +3,6 @@ secuencias decrecientes de números impares. Por ejemplo, si se introduce el
 5 se debe mostrar:"""
 
 numero = int(input("Introduce un número: "))
-mostrar = 1;
 
-for i in range(numero):
-    for j in range(i+1):
-        if mostrar > 9:
-            mostrar = 1;
-        print(mostrar, end=" ")
-        mostrar += 2
-    print()
-
-"""var numero = parseInt(prompt("Introduce un número entero: "));
-var mostrar = 1;
-
-if (isNaN(numero))
-{
-    document.writeln("ERROR: No es un número entero.");
-
-} else 
-{
-    for (let i = 0; i < numero; i++)
-    {
-        for (let j = 0; j < i+1; j++)
-        {
-            if (mostrar > 9)
-            {
-                mostrar = 1;
-            }
-
-            document.writeln(mostrar + " ");
-            mostrar += 2;
-        }
-        document.writeln("<br>");
-    }
-}"""
+for i in range(1, numero + 1):
+    print(" ".join(map(str, range(2 * i - 1, 0, -2))))
