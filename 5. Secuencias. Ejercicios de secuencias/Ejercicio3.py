829adbfa0566008171fe576190c2fd895bfe9a40
Francisco Aybar <frannur1.ir@gmail.com>
2025-10-27 12:36:49 +0100

    Terminado

--- 5. Secuencias. Ejercicios de secuencias/Ejercicio3.py
+++ 5. Secuencias. Ejercicios de secuencias/Ejercicio3.py
@@ -1,5 +1,8 @@
 """ 3. Escribe un programa lea un texto y determine si es un palíndromo. Procura
-crear una función palindromo(s) -> Bool. """
+crear una función palindromo(s) -> Bool. 
+
+NOTA: una cadena es palíndromo si se lee igual de izquierda a derecha que
+de derecha a izquierda. Por ejemplo, la palabra OSO es un palíndromo."""
 
 
 import string
