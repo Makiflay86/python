7f2c6200347d560274f6409cb5d6d643819f0494
Francisco Aybar <frannur1.ir@gmail.com>
2025-11-13 11:50:17 +0100

    e

--- 1. Conceptos Básicos/Ejercicio1.py
+++ 1. Conceptos Básicos/Ejercicio1.py
@@ -1,13 +1,5 @@
-<<<<<<< HEAD
 # 1 Escriba un programa que recoja un valor 
 # por teclado y muestre de qué tipo es.
 
 valor = (input("Introduce un valor: "))
 print("El valor de '" + valor + "' es de tipo: " + str(type(valor)))
-=======
-# 1 Escriba un programa que recoja un valor 
-# por teclado y muestre de qué tipo es.
-
-valor = (input("Introduce un valor: "))
-print("El valor de '" + valor + "' es de tipo: " + str(type(valor)))
->>>>>>> 381deff0e720e097671845d292ab68e004e973d8
