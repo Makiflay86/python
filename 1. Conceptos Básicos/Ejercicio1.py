13cd76655b02d8a96ebce77c2e86880a11f9f492
Francisco Aybar <frannur1.ir@gmail.com>
2025-11-13 16:27:46 +0100

    type

--- 1. Conceptos Básicos/Ejercicio1.py
+++ 1. Conceptos Básicos/Ejercicio1.py
@@ -1,5 +1,21 @@
 # 1 Escriba un programa que recoja un valor 
 # por teclado y muestre de qué tipo es.
 
-valor = (input("Introduce un valor: "))
-print("El valor de '" + valor + "' es de tipo: " + str(type(valor)))
+valor = input("Introduce un valor: ")
+
+try:
+    valor_convertido = int(valor)
+    tipo = type(valor_convertido)
+except ValueError:
+    try:
+        valor_convertido = float(valor)
+        tipo = type(valor_convertido)
+    except ValueError:
+        if valor.lower() in ['true', 'false']:
+            valor_convertido = valor.lower() == 'true'
+            tipo = type(valor_convertido)
+        else:
+            valor_convertido = valor
+            tipo = type(valor_convertido)
+
+print(f"El valor '{valor}' es de tipo: {tipo}")
